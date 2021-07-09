494a8220d6104f4ee503495721fb605c7eb3c798
Junhyung Cho <junhyung@edu.hanbat.ac.kr>
2021-07-09 18:17:49 +0900

    변경된 사항     - networkModule Retrofit
    Resolves: #2
    See also: None

--- app/src/main/java/com/example/onem2m_in_ae/INAEApp.kt
+++ app/src/main/java/com/example/onem2m_in_ae/INAEApp.kt
@@ -1,16 +1,20 @@
 package com.example.onem2m_in_ae
 
 import android.app.Application
+import com.example.onem2m_in_ae.di.getNetworkModule
 import com.example.onem2m_in_ae.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class INAEApp : Application() {
+    val BASE_URL = "192.168.10.0:7081"
+
     override fun onCreate() {
         super.onCreate()
         startKoin {
             androidContext(this@INAEApp)
             viewModelModule
+            modules(getNetworkModule(BASE_URL))
         }
     }
 }
--- app/src/main/java/com/example/onem2m_in_ae/di/networkModule.kt
+++ app/src/main/java/com/example/onem2m_in_ae/di/networkModule.kt
@@ -1,11 +1,43 @@
 package com.example.onem2m_in_ae.di
 
+import com.example.onem2m_in_ae.service.INAEDataService
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.BuildConfig
 import org.koin.dsl.module
+import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 const val BASE_URL = "192.168.10.0:7081"
+
+fun getNetworkModule(baseUrl: String) = module {
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor.Level.BODY
+                } else {
+                    HttpLoggingInterceptor.Level.NONE
+                }
+            })
+            .build()
+    }
+
+    single {
+        GsonConverterFactory.create() as Converter.Factory
+    }
+
+    single {
+        Retrofit.Builder()
+            .client(get())
+            .addConverterFactory(get())
+            .baseUrl(baseUrl)
+            .build()
+            .create(INAEDataService::class.java)
+    }
+}
+
 val networkModule = module {
     single { provideRetrofit(get())}
 }
