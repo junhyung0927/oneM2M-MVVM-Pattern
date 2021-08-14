d493595b2e730674c9b68535b5263d996ac7c993
Junhyung <junhyung@edu.hanbat.ac.kr>
2021-08-14 17:56:17 +0900

    Boiler 리팩토링     - AirConditional 리팩토링과 동일
    Resolves: None
    See also: None

--- app/src/main/java/com/example/onem2m_in_ae/view/ui/activity/BoilerActivity.kt
+++ app/src/main/java/com/example/onem2m_in_ae/view/ui/activity/BoilerActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.example.onem2m_in_ae.R
 import com.example.onem2m_in_ae.databinding.ActivityBoilerBinding
 import com.example.onem2m_in_ae.model.ContainerInstance
+import com.example.onem2m_in_ae.service.mqtt.MqttManager
 import com.example.onem2m_in_ae.view.base.BaseActivity
 import com.example.onem2m_in_ae.view.ui.activity.INAEActivity.Companion.KEY_CONTAINER_DATA
 import com.orhanobut.logger.Logger
@@ -13,8 +14,13 @@ import org.koin.android.viewmodel.ext.android.viewModel
 class BoilerActivity : BaseActivity() {
     private val binding by binding<ActivityBoilerBinding>(R.layout.activity_boiler)
     private val boilerViewModel: BoilerViewModel by viewModel()
+    private val mqttManager: MqttManager by lazy {
+        MqttManager(applicationContext)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding.apply {
             lifecycleOwner = this@BoilerActivity
 
@@ -22,36 +28,46 @@ class BoilerActivity : BaseActivity() {
             val containerItem = intent.getSerializableExtra(KEY_CONTAINER_DATA) as ContainerInstance
             item = containerItem.containerImage
 
-            val RESOURCE_NAME = containerItem.containerInstanceName
+            mqttManager.contentInstanceData.observe(this@BoilerActivity) {
+                sensingDataTextViewBoilerActivity.text = it.con
+            }
 
-            boilerViewModel.getContainerInfo.observe(this@BoilerActivity)
-            {
-                if (RESOURCE_NAME.isNotEmpty()) {
-                    boilerControlModeAppCompactToggleButton.setOnCheckedChangeListener { _, isChecked ->
-                        if (isChecked) {
-                            boilerViewModel.deviceControl("on", RESOURCE_NAME)
-                                .observe(this@BoilerActivity)
-                                { println(it) }
-                        } else {
-                            boilerViewModel.deviceControl("off", RESOURCE_NAME)
-                                .observe(this@BoilerActivity)
-                                { println(it) }
+            boilerViewModel.apply {
+                contentInstanceInfo.observe(this@BoilerActivity) { }
+                contentInstanceControl.observe(this@BoilerActivity) {
+                    println("장치 제어 성공")
+                }
+                deleteContainer.observe(this@BoilerActivity) {
+                    startActivity(Intent(this@BoilerActivity, INAEActivity::class.java))
+                }
+                createSub.observe(this@BoilerActivity) {
+                    println("createSub 성공")
+                    mqttManager.connect(INAEActivity.APP_ID)
+                }
+
+                getChildResourceInfo.observe(this@BoilerActivity) {
+                    val containerResourceName = getResourceName(it)
+                    println("컨테이너 리소스 이름: ${containerResourceName}")
+                    createSubscription(containerResourceName)
+                    getContainerInfo.observe(this@BoilerActivity) {
+                        if (containerResourceName.isNotEmpty()) {
+                            boilerControlModeAppCompactToggleButton.setOnCheckedChangeListener { _, isChecked ->
+                                val content = if (isChecked) {
+                                    "on"
+                                } else {
+                                    "off"
+                                }
+                                deviceControl(content, containerResourceName)
+                            }
                         }
                     }
-                }
-            }
 
-            boilerSearchDataModeAppCompactButton.setOnClickListener {
-                boilerViewModel.getContentInstanceInfo(RESOURCE_NAME).observe(this@BoilerActivity)
-                { Logger.d("보일러 데이터 조회") }
-            }
+                    boilerSearchDataModeAppCompactButton.setOnClickListener {
+                        getContentInstanceInfo(containerResourceName)
+                    }
 
-            boilerDeleteAppCompactToggleButton.setOnClickListener {
-                boilerViewModel.apply {
-                    deleteDataBaseContainer(RESOURCE_NAME).observe(this@BoilerActivity) {
-                        println("해당 장치 데이터베이스 제거 성공")
-                        val intent = Intent(this@BoilerActivity, INAEActivity::class.java)
-                        startActivity(intent)
+                    boilerDeleteAppCompactToggleButton.setOnClickListener {
+                        deleteDataBaseContainer(containerResourceName)
                     }
                 }
             }
